3d34a1b4c366582fed7bbdeb158135c3c18b280a
Markus Köhne <mkoehne@outlook.com>
2019-01-05 13:52:48 +0100

    Firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:ola/services/authentication.dart';
+import 'package:ola/pages/root_page.dart';
 
 void main() {
-  runApp(new SampleApp());
+  runApp(new MyApp());
 }
 
-class SampleApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Sample App',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new SampleAppPage(),
-    );
-  }
-}
-
-class SampleAppPage extends StatefulWidget {
-  SampleAppPage({Key key}) : super(key: key);
-
-  @override
-  _SampleAppPageState createState() => new _SampleAppPageState();
-}
-
-class _SampleAppPageState extends State<SampleAppPage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Ola"),
-      ),
-      body: new ListView(children: _getListData()),
-    );
-  }
-
-  _getListData() {
-    List<Widget> widgets = [];
-    for (int i = 0; i < 100; i++) {
-      widgets.add(new GestureDetector(
-        child: new Padding(
-            padding: new EdgeInsets.all(20.0),
-            child: new Text("Row $i")),
-        onTap: () {
-          print("row tapped $i");
-        },
-      ));
-    }
-    return widgets;
+        title: 'Ola',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
   }
 }
